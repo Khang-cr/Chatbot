19a654317ee137cb497152a7be87dc68611cca28
Ngô Phúc Khang <ngophuckhang2024@gmail.com>
2025-11-13 21:54:19 +0700

    Them therapist vao login/signin

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from .views import (
     SignUpView, 
-    collect_basic_info_view, 
+    collect_basic_info_view,
+    therapist_basic_info_view,
     dass21_test_view,
     dass21_result_view,
-    profile_detail_view
+    profile_detail_view,
+    therapist_verification_view,
+    verification_pending_view,
 )
 
 urlpatterns = [
@@ -13,4 +16,7 @@ urlpatterns = [
     path('dass21-test/', dass21_test_view, name='dass21_test'),
     path('dass21-result/', dass21_result_view, name='dass21_result'),
     path('profile/', profile_detail_view, name='profile_detail'),
+    path('therapist-basic-info/', therapist_basic_info_view, name='therapist_basic_info'),
+    path('therapist-verification/', therapist_verification_view, name='therapist_verification'),
+    path('verification-pending/', verification_pending_view, name='verification_pending'),
 ]
